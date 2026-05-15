3b68cd6233b014a9be456f3b4472e443207fb62a
Siva <sivaavis2799@gmail.com>
2026-05-15 17:18:18 +0530

    Revert "Refactor Redis configuration in initializer; streamline options and enforce SSL usage"
    This reverts commit e83cafb288fbaf52b9c8eead18397b8a163a2c71.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,10 @@
 redis_options = {
   host: ENV.fetch("CACHE_HOST", "localhost"),
-  port: ENV.fetch("CACHE_PORT", 6379).to_i,
-  reconnect_attempts: 3,
-  ssl: true
+  port: ENV.fetch("CACHE_PORT", 6379),
+  ssl: ENV.fetch("CACHE_TLS", "false") == "true"
 }
+
+password = ENV.fetch("CACHE_PASSWORD", nil)
+redis_options[:password] = password if password.present?
 
 REDIS = Redis.new(redis_options)
